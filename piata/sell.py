4e08b3740717f5ed90adf3c5e5c5db285f9d54cf
Jonathan <a0097816@nus.edu.sg>
2013-06-20 17:04:16 +0800

    updated sell module

--- piata/sell.py
+++ piata/sell.py
@@ -9,19 +9,23 @@ from google.appengine.ext import db
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/templates"))
 
+
 class SellPage(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
         template_values = {
             'reminder': 'yay',
             'email': user.email(),
+            'book_form': models.BookForm(),
             'logout': users.create_logout_url(self.request.host_url),
             }
         template = jinja_environment.get_template('sell.html')
         self.response.out.write(template.render(template_values))
 
+
 class Submit(webapp2.RequestHandler):
     def post(self):
+        user = users.get_current_user()
         module = self.request.get('module').rstrip()
         title = self.request.get('title').rstrip()
         author = self.request.get('author').rstrip()
@@ -29,7 +33,7 @@ class Submit(webapp2.RequestHandler):
         edition = self.request.get('edition').rstrip()
         cost = self.request.get('cost').rstrip()
         comments = self.request.get('comments').rstrip()
-        zxc = self.request.get('asd', allow_multiple=True)
+        zxc = self.request.get('conditions', allow_multiple=True)
 
         for a in zxc:
             if a == 'not_used_once':
@@ -37,21 +41,18 @@ class Submit(webapp2.RequestHandler):
             else:
                 condition = 3
 
-
-
-        user = users.get_current_user()
         template_values = {
-            'module' : module,
-            'title' : title,
-            'author' : author,
-            'publisher' : publisher,
-            'edition' : edition,
-            'cost' : cost,
-            'comments' : comments,
-            'condition' : condition,
-            'email' : user.email(),
-            'logout' : users.create_logout_url(self.request.host_url),
-            'asd' : zxc,
+            'module': module,
+            'title': title,
+            'author': author,
+            'publisher': publisher,
+            'edition': edition,
+            'cost': cost,
+            'comments': comments,
+            'condition': condition,
+            'email': user.email(),
+            'logout': users.create_logout_url(self.request.host_url),
+            'asd': zxc,
             }
         template = jinja_environment.get_template('submit.html')
         self.response.out.write(template.render(template_values))
